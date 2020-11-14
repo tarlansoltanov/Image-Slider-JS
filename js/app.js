cd271b42947e80ae1005699357c76cdaaf55b9d3
Tarlan Soltanov <soltanov.terlan04@outlook.com>
2020-11-14 16:14:58 +0400

    Loop Sliding Ready

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 const sliderPhotos = document.querySelector('.slider-photos');
 const sliderContainer = document.querySelector('.slider-container');
 
+const previousBtn = document.querySelector('#previousBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
+const size = sliderContainer.clientWidth/4;
 
 var photos = ['img1.jpg','img2.jpg','img3.jpg','img4.jpg','img5.jpg', 'img6.jpg', 'img7.jpg', 'img8.jpg']
 
@@ -13,3 +17,23 @@ window.onload = () =>{
         sliderPhotos.innerHTML += img_template(photos[i]);
     }
 }
+
+let counter = 0;
+
+nextBtn.addEventListener('click', ()=>{
+    sliderPhotos.style.transition = 'transform 0.2s ease-in-out';
+    counter++;
+    if (counter>photos.length-4){
+        counter=0;
+    }
+    sliderPhotos.style.transform="translateX(-"+counter*size+"px)";
+})
+
+previousBtn.addEventListener('click', ()=>{
+    sliderPhotos.style.transition = 'transform 0.2s ease-in-out';
+    counter--;
+    if (counter<0){
+        counter=photos.length-4;
+    }
+    sliderPhotos.style.transform="translateX(-"+counter*size+"px)";
+})
